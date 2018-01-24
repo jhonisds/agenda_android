d827dd1369a1294826f73990b0ab136904187819
jhonisds <jhoni.sousa@fatec.sp.gov.br>
2018-01-24 00:05:16 -0200

    Output cam

--- app/src/main/java/br/com/rbg/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/rbg/agenda/FormularioActivity.java
@@ -1,12 +1,18 @@
 package br.com.rbg.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
 
 import br.com.rbg.agenda.dao.AlunoDAO;
 import br.com.rbg.agenda.model.Aluno;
@@ -28,6 +34,18 @@ public class FormularioActivity extends AppCompatActivity {
         if (aluno != null) {
             helper.preencheFormulario(aluno);
         }
+
+        Button btnFoto = findViewById(R.id.frm_btn_foto);
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() +".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivity(intentCamera);
+            }
+        });
 
     }
 
